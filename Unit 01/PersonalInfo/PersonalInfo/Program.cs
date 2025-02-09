8d6682d5357f1f9619ce614dd8f351c309d166cb
unknown <zb4963kw@go.minnstate.edu>
2025-02-09 13:57:00 -0600

    First Commit

---
+++ Unit 01/PersonalInfo/PersonalInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+class PersonalInfo
+{
+    static void Main(string[] args)
+    {
+        WriteLine("Name: Jessica Brown");
+        WriteLine("Birthdate: January 31, 1988");
+        WriteLine("Work Phone Number: (999)999-9999");
+        WriteLine("Cell Phone Number: (888)888-8888");
+
+    }
+}
+
